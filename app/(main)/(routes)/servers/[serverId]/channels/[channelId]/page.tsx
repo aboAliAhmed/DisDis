86280f9ab1e0c37a1621cb87b99f95a81a644a7e
aboAliAhmed <ahmedbeasar@gmail.com>
2024-06-05 16:19:21 +0300

    complete redirecting to default channel && chat Header

---
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,50 @@
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    }
+}
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirect ('/api/sign-in')
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        }
+    })
+
+    const member = await db.member.findFirst ({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
+        }
+    })
+
+    if(!channel || !member) {
+        redirect("/");
+    }
+
+    return ( 
+        <div className="bg-white dark:bg-zinc-800/20 flex flex-col h-full">
+            <ChatHeader 
+              name={channel.name}
+              serverId={channel.serverId}
+              type="channel"
+            />
+        </div>
+    );
+}
+ 
+export default ChannelIdPage;
