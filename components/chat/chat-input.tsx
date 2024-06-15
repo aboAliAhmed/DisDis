f1b6ab4eec4f1e65d35fe63f0a8d8b974783c3ea
aboAliAhmed <ahmedbeasar@gmail.com>
2024-06-15 09:34:40 +0300

    Added direct messages

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -23,7 +23,7 @@ interface ChatInputProps {
     apiUrl: string;
     query: Record<string, any>;
     name: string;
-    type: "converstaion" | "channel";
+    type: "conversation" | "channel";
 }
 
 const formSchema = z.object({
@@ -81,7 +81,7 @@ export const ChatInput = ({
                                 </button>
                                 <Input 
                                   disabled={isLoading}
-                                  placeholder={`Message ${type === "converstaion" ? name : "#" + name}`}
+                                  placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                                   {...field}
                                   className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                                 />
